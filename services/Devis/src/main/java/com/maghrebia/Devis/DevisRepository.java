b7b5c96f2cb3cc33c7dadbd6329eceb610597d98
yossra-t <yossra.tlili123456789@gmail.com>
2025-03-05 12:28:04 +0100

    DevisRepository.java

--- services/Devis/src/main/java/com/maghrebia/Devis/DevisRepository.java
+++ services/Devis/src/main/java/com/maghrebia/Devis/DevisRepository.java
@@ -1,6 +1,18 @@
 package com.maghrebia.Devis;
 
+import com.maghrebia.Devis.Devis;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
-public interface DevisRepository extends JpaRepository<Devis,Integer> {
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface DevisRepository extends MongoRepository<Devis, String> {
+    Optional<Devis> findById(String id);
+
+    void deleteById(String id);
+    // Custom query to find all Devis by Client ID
+//         List<Devis> findByClient_NumClient(int clientId);
 }
